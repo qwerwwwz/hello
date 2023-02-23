84aaa97c85176f8aa28cee48d36be47ef473c947
qwerwwwz <116714628+qwerwwwz@users.noreply.github.com>
2023-02-23 16:44:15 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+import json
+file = open('name.txt','w')
+names = ['zhangsan', 'lisi', 'wangwu', 'jerry', 'henry', 'merry', 'chris']
+
+result = json.dumps(names)
+
+file.write(result)
+
+file.close()
